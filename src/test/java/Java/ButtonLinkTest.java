664cf75e9685fdb0baad0bc611b36e64e6a2395c
Mehak Malhotra <mehakmalhotra86659@gmail.com>
2022-02-04 15:58:21 +0530

    Commit

---
+++ src/test/java/Java/ButtonLinkTest.java
@@ -0,0 +1,25 @@
+package Java;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+    public class ButtonLinkTest
+    {
+        public static void main(String args[])
+        {
+            WebDriver driver;
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://the-internet.herokuapp.com/");
+            driver.manage().window().maximize();
+            driver.findElement(By.linkText("Inputs")).click();
+            driver.findElement(By.xpath("//input[@type='number']")).sendKeys(
+                    "12345");
+
+        }
+    }
+
---
+++ src/test/java/Java/Checkboxtest.java
@@ -0,0 +1,24 @@
+package Java;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+    import java.util.List;
+
+    public class Checkboxtest {
+        public static void main(String[] args) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://the-internet.herokuapp.com/");
+            driver.findElement(By.linkText("Checkboxes")).click();
+
+            List<WebElement> lst = driver.findElements(By.xpath("//input[@type='checkbox']"));
+            lst.get(0).click();
+            System.out.println(lst.size());
+        }
+    }
+
